7698022b22382431ce39d0a6722b71b2141edc9a
Monir <mrmonir0558@gmail.com>
2025-01-23 23:37:53 +0600

    test5

---
+++ app/components/AuthProvider.jsx
@@ -0,0 +1,7 @@
+'use client';
+import { KindeProvider } from '@kinde-oss/kinde-auth-nextjs';
+
+const AuthProvider = ({ children }) => {
+    return <KindeProvider>{children}</KindeProvider>;
+};
+export default AuthProvider;
