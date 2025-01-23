f1f571eeffcf09cf808965b2fe2f9fc1ac751ce5
Monir <mrmonir0558@gmail.com>
2025-01-23 20:35:27 +0600

    some changed in the profile pages

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Profile = async () => {
-    
     const { isAuthenticated, getUser } = getKindeServerSession();
     const isUserAuthenticated = await isAuthenticated();
 
@@ -12,22 +11,24 @@ const Profile = async () => {
     return (
         <div className="text-center mt-20 text-2xl px-3">
             <div className="max-w-xl mx-auto bg-gray-100 py-20 rounded-md">
-                {isUserAuthenticated ? (
-                    <div className="flex flex-col mx-auto justify-center items-center gap-6">
-                        <h1>Welcome to my Profile</h1>
-                        <Image src={user.picture || ''} alt={user.given_name || ''} width={100} height={100} />
-                        <h3 className="text-sm font-bold">
-                            {user.given_name} {user.family_name}
-                        </h3>
-                        <Link href="/api/auth/logout" className="hover:text-orange-600">
-                            <button className="btn text-base"> Sign Out</button>
-                        </Link>
-                    </div>
+                {/* {isUserAuthenticated ? (
+                    
                 ) : (
                     <div>
                         <h1>Forbidden</h1>
                     </div>
-                )}
+                )} */}
+
+                <div className="flex flex-col mx-auto justify-center items-center gap-6">
+                    <h1>Welcome to my Profile</h1>
+                    <Image src={user.picture || ''} alt={user.given_name || ''} width={100} height={100} />
+                    <h3 className="text-sm font-bold">
+                        {user.given_name} {user.family_name}
+                    </h3>
+                    <Link href="/api/auth/logout" className="hover:text-orange-600">
+                        <button className="btn text-base"> Sign Out</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
