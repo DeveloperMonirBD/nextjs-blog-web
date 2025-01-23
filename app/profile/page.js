07cfc070cdb06c4cf9926c043d66ce0fde362bca
Monir <mrmonir0558@gmail.com>
2025-01-23 22:23:29 +0600

    test4

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,19 +1,17 @@
-'use client';
-import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Profile = () => {
-    console.log("profile")
-    const { isAuthenticated, getUser } = useKindeBrowserClient();
-    console.log(isAuthenticated)
-    const user = getUser();
-    console.log(user)
+const Profile = async () => {
+    const { isAuthenticated, getUser } = getKindeServerSession();
+    const isUserAuthenticated = await isAuthenticated();
+    console.log(isAuthenticated);
+    const user = await getUser();
 
     return (
         <div className="text-center mt-20 text-2xl px-3">
             <div className="max-w-xl mx-auto bg-gray-100 py-20 rounded-md">
-                {isAuthenticated ? (
+                {isUserAuthenticated ? (
                     <div className="flex flex-col mx-auto justify-center items-center gap-6">
                         <h1>Welcome to my Profile</h1>
                         <Image src={user.picture || ''} alt={user.given_name || ''} width={100} height={100} />
