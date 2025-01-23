a32a5af30aef5d339f9d738c44e495a136c99600
Monir <mrmonir0558@gmail.com>
2025-01-23 09:42:39 +0600

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+
+
+const Navbar = async() => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+    console.log(user)
+    return (
+        <div className="bg-gray-200 text-gray-700">
+            <nav className="container mx-auto flex justify-between py-4">
+                <div>
+                    <h1>Logo</h1>
+                </div>
+                <div className="space-x-6">
+                    <Link href="/" className="hover:text-orange-600">
+                        Home
+                    </Link>
+                    <Link href="/profile" className="hover:text-orange-600">
+                        Profile
+                    </Link>
+                    {user ? (
+                        <>
+                            <Link href="/api/auth/logout" className="hover:text-orange-600">
+                                Sign Out
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/api/auth/login" className="hover:text-orange-600">
+                                Sign In
+                            </Link>
+                            <Link href="/api/auth/register" className="hover:text-orange-600">
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
